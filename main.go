59af0c851f2c9a8efd0ba34a013afbf3ad7a5316
liamchampton <liam.hampton@hotmail.co.uk>
2020-11-17 19:18:51 +0000

    cobra cli initialised

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import "fmt"
+import "github.com/template-cli/cmd"
 
 func main() {
-	fmt.Println("Hello, World")
+	cmd.Execute()
 }
